296e66f546d29e0d68cbf4607a9276ac2dd67624
Phillip <your.email@example.com>
2024-04-03 22:52:56 -0400

    Router changes, login script, and changes to wiring of pages

--- routes/root.js
+++ routes/root.js
@@ -30,4 +30,13 @@ router.get('^/$|/login(.html)?', (req, res) => {
 router.get('^/$|/login(.css)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'login.css'));
 });
+router.get('^/$|/login(.js)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'login.js'));
+});
+router.get('^/$|/register(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'register.html'));
+});
+router.get('^/$|/register(.css)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'register.css'));
+});
 module.exports = router;
