811affad9e2943a10f2359510fe3c6e512ffef21
Phillip <your.email@example.com>
2024-04-08 20:15:47 -0400

    Damian CSS updates

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'https://fitnessfinalproject.glitch.me',
     'http://localhost:5500',
+    'http://localhost:3000',
     'http://127.0.0.1:5500'
 ];
 
--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,7 @@
-const form = document.getElementById('loginSubmit');
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('loginForm');
   //Listens for when the submit button is clicked.
   form.addEventListener('submit', function(event) {
   event.preventDefault(); // Prevent default form submission
   window.location.href = 'fitnessTest.html';});
+  });
